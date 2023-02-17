cd8c4cc7360b3fb4f22624a98e9870a1cef8771c
William Barnes <jakebarnes1989@gmail.com>
2023-02-16 18:16:59 -0800

    Implemented mongodb connection, need to add CRUD. Updated Slack event processing to work with IMs and channel joins for private conversation groups and to ignore channel kicks.

---
+++ mongodb/mongo.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@aiconversationcluster0.4tyg45o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+      const database = client.db('sample_mflix');
+      const movies = database.collection('movies');
+      // Query for a movie that has the title 'Back to the Future'
+      const query = { title: 'Back to the Future' };
+      const movie = await movies.findOne(query);
+      console.log(movie);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
--- slack/process-event.js
+++ slack/process-event.js
@@ -3,10 +3,9 @@ const slackMessage = require('./process-message');
 // Process the Slack event for the event type and call the appropriate function
 // Currently only processing messages is supported
 async function processEvent(req) {
-    console.log(`Processing event: ${req}`);
 
     // Check if the event is a message
-    if (req.body.event.type === 'message') {
+    if (req.body.event.type === 'message' && req.body.event.user !== 'USLACKBOT' && req.body.event.subtype === undefined || req.body.event.subtype === 'channel_join') {
 
         // Process the message
         await slackMessage.processMessage(req);
