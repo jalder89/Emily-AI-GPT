3a54599f32b0de176c031b6dd7ec2991aba6d168
William Barnes <jakebarnes1989@gmail.com>
2023-02-18 11:55:14 -0800

    Cleaned up documentation and formatting. Removed debug console.logs

--- openai/memory.js
+++ openai/memory.js
@@ -1,27 +1,21 @@
 let memory = [];
 
 async function addToMemory(prompt, completion) {
-
     // Add the prompt and completion to memory
     memory.push({
         prompt: prompt,
         completion: completion,
         count: memory.length + 1
     });
-
 }
 
 // Return the memory array to the caller
 async function getMemory() {
-
     return memory;
-
 }
 
 async function clearMemory() {
-
     memory = [];
-
 }
 
 // Export the functions
--- slack/process-event.js
+++ slack/process-event.js
@@ -3,15 +3,11 @@ const slackMessage = require('./process-message');
 // Process the Slack event for the event type and call the appropriate function
 // Currently only processing messages is supported
 async function processEvent(req) {
-
     // Check if the event is a message
     if (req.body.event.type === 'message' && req.body.event.user !== 'USLACKBOT' && req.body.event.subtype === undefined || req.body.event.subtype === 'channel_join') {
-
         // Process the message
         await slackMessage.processMessage(req);
-
     }
-
 }
 
 // Export the function
