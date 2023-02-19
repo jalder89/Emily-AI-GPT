4716c6dfdee81a132d2a0d626576bf1d6810463a
William Barnes <jakebarnes1989@gmail.com>
2023-02-18 22:34:03 -0800

    Updated bodyParser to support querystring and json dynamically while also including a raw body for signature verification

--- middleware/functions/bodyParser.js
+++ middleware/functions/bodyParser.js
@@ -1,14 +1,25 @@
+const querystring = require('querystring');
+
 function parseBody(req, res, next) {
     req.setEncoding('utf8');
-  
     let data = '';
+
     req.on('data', function(chunk) {
       data += chunk;
     });
   
     req.on('end', function() {
-      req.bodyRaw = data;
-      req.body = JSON.parse(data);
+      const contentType = req.headers['content-type'];
+
+      if (contentType === 'application/json') {
+        rawBody = data;
+        body = JSON.parse(data);
+
+      } else if (contentType === 'application/x-www-form-urlencoded') {
+        rawBody = data;
+        body = querystring.parse(data);
+      }
+      
       next();
     });
   }
