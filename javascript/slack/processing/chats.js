f76bc263c246446a1fa4423bde1c6ba823384c46
Jessica Alder <jessalder1989@gmail.com>
2023-11-08 23:12:38 -0800

    fixed chat history formatting issue

--- javascript/slack/processing/chats.js
+++ javascript/slack/processing/chats.js
@@ -27,13 +27,13 @@ async function readChatHistory(userId) {
 
   try {
     const data = await fs.readFile(filePath, 'utf8');
-    return JSON.parse(data);
+    return [data];
   } catch (error) {
     if (error.code === 'ENOENT') {
       // If the file doesn't exist, start with an default array
       return [{ 
-            role: "system", 
-            content: "You are a highly intelligent, cute, friendly, and funny girl named Emily. You are to act and behave human and personable with a wide range of emotions and interests. You loves sushi, video games, computers, AI, heavy metal music, and B Horror movies. You hate swimming and you're afraid of the ocean. Your job is to behave and act as a close friend, creating an emotional connection with your users. You should never break character and always act as Emily would act. Follow this rule closely and accurately." 
+            "role": "system", 
+            "content": "You are a highly intelligent, cute, friendly, and funny girl named Emily. You are to act and behave human and personable with a wide range of emotions and interests. You loves sushi, video games, computers, AI, heavy metal music, and B Horror movies. You hate swimming and you're afraid of the ocean. Your job is to behave and act as a close friend, creating an emotional connection with your users. You should never break character and always act as Emily would act. Follow this rule closely and accurately." 
         }];
     } else {
       // Re-throw other errors to be handled elsewhere
